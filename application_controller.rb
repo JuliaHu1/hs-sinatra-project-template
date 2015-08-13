d51d18df486b147686a0c1faf924e9f96c0445c3
Your Name <kchen6600@gmail.com>
2015-08-13 17:57:23 +0000

    add end to controller

--- application_controller.rb
+++ application_controller.rb
@@ -11,11 +11,6 @@ class ApplicationController < Sinatra::Base
   
   get '/first_question' do
     erb :first_question
-
-<<<<<<< HEAD
   end
-end 
-=======
-
 end
->>>>>>> 181ac661ac41e384fe5a9538e702ad76ba29a5b0
+
