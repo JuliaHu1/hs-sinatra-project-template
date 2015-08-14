9f65b77d678ac28e6a156b943ab361b02518d63d
Devin <devin.milberg@gmail.com>
2015-08-14 13:31:10 +0000

    control page

--- application_controller.rb
+++ application_controller.rb
@@ -16,6 +16,17 @@ class ApplicationController < Sinatra::Base
   get '/second_question' do
     erb :second_question
   end
+
+  get '/third_question' do
+    erb :third_question
+  end
+  
+  get 'fourth_question' do
+    erb :fourth_question
+  end
+  
+  get '/fifth_question' do
+    erb :fifth_question
+  end
+
 end
-
-
